263dbb518b6be6ef49e9cf05082ca54731ed93ac
Ivan Seleznev <vanesso1389@gmail.com>
2024-08-13 14:52:15 +0300

    LALT(KC_SPACE) turns off layer 1

--- keyboards/ergohaven/k03/keymaps/v1/keymap.c
+++ keyboards/ergohaven/k03/keymaps/v1/keymap.c
@@ -64,6 +64,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         case KC_NUM:
             return true;
+        case LALT(KC_SPACE):
+            layer_off(1);
+            return true;
         default:
             if (record->event.pressed) {
                 if(numlock_enabled) {
