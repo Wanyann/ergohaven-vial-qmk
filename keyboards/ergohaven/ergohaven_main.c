8a79206bbeb0695ac000ee869b6b3f3fc797025e
Ivan Seleznev <vanesso1389@gmail.com>
2025-03-11 15:01:22 +0300

    fix

--- keyboards/ergohaven/ergohaven_main.c
+++ keyboards/ergohaven/ergohaven_main.c
@@ -50,10 +50,10 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
     switch (detected_os) {
         case OS_MACOS:
         case OS_IOS:
-            tap_code(CG_LSWP);
+            tap_code16(CG_LSWP);
             break;
         case OS_WINDOWS:
-            tap_code(CG_LNRM);
+            tap_code16(CG_LNRM);
             break;
         case OS_LINUX:
         case OS_UNSURE:
