6a6eb288662566d7984d70bfd3b3441548644b0f
Ivan Seleznev <vanesso1389@gmail.com>
2025-03-13 16:16:07 +0300

    Update ergohaven_pointing.c

--- keyboards/ergohaven/ergohaven_pointing.c
+++ keyboards/ergohaven/ergohaven_pointing.c
@@ -240,9 +240,10 @@ report_mouse_t pointing_device_task_user(report_mouse_t mrpt) {
     
     #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
     is_mouse_active = abs(mrpt.x) > 1 || abs(mrpt.y) > 1 || abs(mrpt.v) > 1 || abs(mrpt.h) > 1 || mrpt.buttons;
+    if(is_mouse_active) uprintf("mrpt.x: %u ; mrpt.y: %u ; mrpt.h: %u ; mrpt.v: %u \n", mrpt.x, mrpt.y, mrpt.h, mrpt.v); 
     #endif
     pointing_mode_t pmode = pointing_mode;
-    if(is_mouse_active) uprintf("mrpt.x: %u ; mrpt.y: %u ; mrpt.h: %u ; mrpt.v: %u \n", mrpt.x, mrpt.y, mrpt.h, mrpt.v); 
+    
     // dealing with two finger gesture on touch
     if (mrpt.h != 0 || mrpt.v != 0) {
         pmode  = POINTING_MODE_SCROLL;
