374c64e6e223363315c60062cbe50198870c7f9b
Ivan Seleznev <vanesso1389@gmail.com>
2025-07-23 11:27:21 +0300

    Update ergohaven_main.c

--- keyboards/ergohaven/ergohaven_main.c
+++ keyboards/ergohaven/ergohaven_main.c
@@ -369,6 +369,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LCTL(KC_F):
             layer_off(_NAV);
             layer_off(_MOUSE);
+            auto_mouse_layer_off();
             return true;
 
         case LT(5, KC_SPACE):
