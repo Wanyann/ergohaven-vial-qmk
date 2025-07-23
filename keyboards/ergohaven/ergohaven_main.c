dd1fbd6002d589aafd26bf0338c362ea11071764
Ivan Seleznev <vanesso1389@gmail.com>
2025-07-23 11:26:38 +0300

    Update ergohaven_main.c

--- keyboards/ergohaven/ergohaven_main.c
+++ keyboards/ergohaven/ergohaven_main.c
@@ -314,6 +314,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if(IS_LAYER_ON(_MCTRL))
         {
             layer_off(_MOUSE);
+            auto_mouse_layer_off();
         }
     }
 
